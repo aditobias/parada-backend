b55a8e8ee1d1c4eceafdd87506f111408e2c3eb1
ADRIAN TOBIAS (SHP-IRIS-ISD-OOCLL/MNL) <TOBIAAD@OOCL.COM>
2019-10-25 07:30:25 +0800

    Added default Spring Boot

---
+++ src/main/java/com/example/paradabackend/ParadaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.paradabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParadaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParadaBackendApplication.class, args);
+	}
+
+}
