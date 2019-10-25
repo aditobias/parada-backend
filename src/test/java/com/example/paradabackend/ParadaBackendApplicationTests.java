520b3f0cdac16588648e013707ac5ca2fe2c175d
IYAN OLIVER ALVAREZ (PLAN-IRIS-ISD-OOCLL/MNL) <ALVARIY@OOCL.COM>
2019-10-25 13:52:24 +0800

    Fixed error in ParadaBackendApplicationTests

--- src/test/java/com/example/paradabackend/ParadaBackendApplicationTests.java
+++ src/test/java/com/example/paradabackend/ParadaBackendApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ParadaBackendApplicationTests {
+public class ParadaBackendApplicationTests {
 
 	@Test
 	public void contextLoads() {
