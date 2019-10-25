dc82f82909b4d1eb422fa0445653b8521adf6089
jed1337 <j.caychingco@Yahoo.com>
2019-10-25 13:54:40 +0800

    [Jed and Gray] Create driver controller

---
+++ src/main/java/com/example/paradabackend/controllers/DriverController.java
@@ -0,0 +1,22 @@
+package com.example.paradabackend.controllers;
+
+import com.example.paradabackend.entities.Driver;
+import com.example.paradabackend.services.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/drivers")
+public class DriverController {
+    @Autowired
+    private DriverService driverService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(code = HttpStatus.OK)
+    public Driver getDriverByUserNameAndPassword(@RequestBody Driver driver) {
+        return driverService.findByUserNameAndPassword(driver.getUserName(), driver.getPassword());
+    }
+
+}
