c5daaaecabb7234fb461a4682fe4537df7ed3ef3
GRAY JEANNE MEDINA (SHP-IRIS-ISD-OOCLL/MNL) <MEDINGR2@OOCL.COM>
2019-10-28 21:38:48 +0800

    [GRAY & ADI] Finished Update on Start for Admin

--- src/test/java/com/example/paradabackend/services/ParkingSpaceServiceTest.java
+++ src/test/java/com/example/paradabackend/services/ParkingSpaceServiceTest.java
@@ -5,20 +5,24 @@ import com.example.paradabackend.entities.ParkingSpace;
 import com.example.paradabackend.repositories.ParkingLotRepository;
 import com.example.paradabackend.repositories.ParkingSpaceRepository;
 import javassist.NotFoundException;
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -32,6 +36,7 @@ class ParkingSpaceServiceTest {
 
     @MockBean
     private ParkingLotRepository parkingLotRepository;
+
 
     private ParkingSpace dummyParkingSpace(String id, String parkingLotName) {
         ParkingSpace parkingSpace = new ParkingSpace();
@@ -88,5 +93,21 @@ class ParkingSpaceServiceTest {
         List<ParkingSpace> resultingParkingSpaceList = parkingSpaceService.findAllByParkingLotName(parkingLotName);
 
         assertThat(resultingParkingSpaceList, hasSize(parkingSpaceList.size()));
+    }
+
+    @Test
+    public void should_update_startTime_when_Payment_is_confirmed () {
+
+        String parkingSpaceID = "PA-1A1";
+        ParkingSpace parkingSpace = new ParkingSpace();
+        parkingSpace.setId(parkingSpaceID);
+        parkingSpace.setStartTime(new Timestamp(System.currentTimeMillis()));
+
+        ParkingSpace updatedParkingSpace = new ParkingSpace();
+        updatedParkingSpace.setId("PA-1A1");
+        when(parkingSpaceRepository.findById(eq("PA-1A1"))).thenReturn(Optional.of(parkingSpace));
+        when(parkingSpaceRepository.save(any())).thenReturn(updatedParkingSpace);
+
+        assertThat(updatedParkingSpace, is(parkingSpaceService.updateStartTimeWhenPaid(parkingSpaceID)));
     }
 }
